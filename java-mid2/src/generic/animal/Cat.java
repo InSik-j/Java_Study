88e01654f3432bd31fb4b2f8819d200270b4dea3
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-05-27 10:54:09 +0900

    제네릭 - Generic 1 / 제네릭 활용 예제

---
+++ java-mid2/src/generic/animal/Cat.java
@@ -0,0 +1,12 @@
+package generic.animal;
+
+public class Cat extends Animal{
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+    
+    @Override
+    public void sound(){
+        System.out.println("냐옹");
+    }
+}
