9984c6df20a2a046d419344a346d5a97a0717d40
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-05-27 09:49:03 +0900

    제네릭 - Generic 1 / 제네릭이 필요한 이유

---
+++ java-mid2/src/generic/ex1/IntegerBox.java
@@ -0,0 +1,12 @@
+package generic.ex1;
+
+public class IntegerBox {
+    public Integer value;
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+    public Integer get() {
+        return value;
+    }
+}
